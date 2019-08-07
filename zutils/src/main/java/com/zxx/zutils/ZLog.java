f797d0aad26eca0a83b246e1b1c83b71af58490e
Wali Zh <2436994090@qq.com>
2019-08-07 18:26:20 +0800

    Update ZLog.java

--- zutils/src/main/java/com/zxx/zutils/ZLog.java
+++ zutils/src/main/java/com/zxx/zutils/ZLog.java
@@ -13,7 +13,7 @@ public class ZLog {
         throw new UnsupportedOperationException("cannot be instantiated");
     }
 
-    public static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
+    public static boolean isDebug = false;// 是否需要打印bug，可以在application的onCreate函数里面初始化
     private static final String TAG = "ZLog";
 
     /*********----- 下面四个是默认tag的函数 ---------**************/
